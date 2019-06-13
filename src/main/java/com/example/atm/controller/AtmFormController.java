971a0f7940456050377adc27f776c91e1e46ee71
nipon pantong <nipon.pantong@scb.co.th>
2019-06-14 01:20:11 +0700

    Adding init notes form endpoint

---
+++ src/main/java/com/example/atm/controller/AtmFormController.java
@@ -0,0 +1,14 @@
+package com.example.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/form")
+public class AtmFormController {
+
+    @GetMapping("/initNotes")
+    public String showInitNotesForm() {
+        return "initNote";
+    }
+}
