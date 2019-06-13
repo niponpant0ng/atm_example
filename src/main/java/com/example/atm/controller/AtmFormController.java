ae7e69dbf3ebfe04d1da4fb29066b5180b878964
nipon pantong <nipon.pantong@scb.co.th>
2019-06-14 01:57:52 +0700

    Edit swagger desc of atm form controller

--- src/main/java/com/example/atm/controller/AtmFormController.java
+++ src/main/java/com/example/atm/controller/AtmFormController.java
@@ -1,10 +1,12 @@
 package com.example.atm.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/form")
+@Api(description = "ATM Form", tags = "Form")
 public class AtmFormController {
 
     @GetMapping("/initNotes")
